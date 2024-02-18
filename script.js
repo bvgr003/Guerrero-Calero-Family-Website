5dbecef4aa00585063ea41f1d8b3683329745389
bvgr003 <148019599+bvgr003@users.noreply.github.com>
2024-02-18 08:14:22 -0600

    indexes Update

--- script.js
+++ script.js
@@ -57,10 +57,4 @@
      updateImage();
  }
 
- function openNav() {
-     document.getElementById("myNav").style.width = "100%";
- }
 
- function closeNav() {
-     document.getElementById("myNav").style.width = "0%";
- }
